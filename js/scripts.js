b0ca157158e8889897acdc0bde9102871af8ce1a
Dave Charnuska <davecharnuska@gmail.com>
2015-12-30 10:57:20 -0500

    clean up, break into smaller functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+	stories = []
+	
 	var camperNewsAPI = "http://www.freecodecamp.com/news/hot";
 	
 	function createDiscussLink(title) {
@@ -9,37 +11,45 @@ $(document).ready(function(){
 	function createUserLink(uname) {
 		return "http://www.freecodecamp.com/" + uname;
 	}
-
-	$.getJSON( camperNewsAPI, {
-	    format: "json"
-	  }).done(function( data ) {
-	      data.forEach(function(item) {
-	      	var author = item.author,
-							uname = author.username,
-							userLink = createUserLink(uname),
-							headline = item.headline,
-							image = (item.image || author.picture),
-							link = item.link,
-							rank = item.rank,
-							discuss = item.storyLink,
-							discussLink = createDiscussLink(discuss),
-							date = item.timePosted,
-							date = new Date(date),
-					date = date.toDateString();
-							
-					var html = "<div class='story-container'>" +
-							"<div class='user-image'><a href='" + link + "' target='_blank'><img src='" + image + "'></a></div>" +
-							"<div class='user-info'>" +
-							"<div class='headline'><a href='" + link + "' target='_blank'>" + headline + "</a></div>" +
-							"<div class='author'><a href='" + userLink + "' target='_blank'>by - " + uname + "</a></div>" +
-							"<div class='like-container'>" +
-							"<div class='like'><p>" + rank + "</p></div>" +
-							"<div class='discuss'><a href='" + discussLink + "' target='_blank'>Discuss</a></div>" +
-							"</div>" +
-							"<div class='date'><p>Posted on: " + date + "</p></div>" +
-							"</div></div>"
-							
-					$("#stories").append(html);
-	      });
-	    });
+	
+	function addStory(item) {
+  	var author = item.author,
+				uname = author.username,
+				userLink = createUserLink(uname),
+				headline = item.headline,
+				image = (item.image || author.picture),
+				link = item.link,
+				rank = item.rank,
+				discuss = item.storyLink,
+				discussLink = createDiscussLink(discuss),
+				date = item.timePosted,
+				date = new Date(date),
+		date = date.toDateString();
+				
+		var html = "<div class='story-container'>" +
+				"<div class='user-image'><a href='" + link + "' target='_blank'><img src='" + image + "'></a></div>" +
+				"<div class='user-info'>" +
+				"<div class='headline'><a href='" + link + "' target='_blank'>" + headline + "</a></div>" +
+				"<div class='author'><a href='" + userLink + "' target='_blank'>by - " + uname + "</a></div>" +
+				"<div class='like-container'>" +
+				"<div class='like'><p>" + rank + "</p></div>" +
+				"<div class='discuss'><a href='" + discussLink + "' target='_blank'>Discuss</a></div>" +
+				"</div>" +
+				"<div class='date'><p>Posted on: " + date + "</p></div>" +
+				"</div></div>"
+				
+		$("#stories").append(html);
+	}
+	
+	function checkStories() {
+		$.getJSON( camperNewsAPI, {
+		    format: "json"
+		  }).done(function( data ) {
+		      data.forEach(function(item) {
+		      	addStory(item);
+		      });
+		    });
+	}
+	
+	checkStories();
 });
